5000445044da7646e786c9d6e1a5838dd77882c0
ABinashjena <38462211+ABinashjena@users.noreply.github.com>
2018-05-18 12:22:15 +0530

    no 9

---
+++ src/javalogic/Howtofindsumofalldigitofanumber9.java
@@ -0,0 +1,27 @@
+package javalogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Howtofindsumofalldigitofanumber9 {
+
+	public static void main(String[] args) {
+		
+		
+		Integer in[]= {1,2,3,4,5,0};
+		int len = in.length;
+		List<Integer> l= new ArrayList<Integer>();
+		for(int i:in)
+		{
+			l.add(i);
+		}
+		System.out.println(l);
+		int sum=0;
+for(int var: l)
+{		
+	sum=sum+var;
+}
+	System.out.println(sum);	
+	}
+
+}
