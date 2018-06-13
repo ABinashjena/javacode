c5aefbeeeba076e8c63b7fc42178ba828e743eac
ABinashjena <38462211+ABinashjena@users.noreply.github.com>
2018-06-13 15:12:25 +0530

    13-06

---
+++ src/javalogic/Test.java
@@ -0,0 +1,37 @@
+package javalogic;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++)
+		{
+			
+	        if(i%3==0)
+			{
+				System.out.println("Abinash");
+			}
+	        else if(i%5==0)
+	        {
+	        	System.out.println("jena");
+	        }
+	        else if(i%2==0)
+	        {
+	        	System.out.println("papu");
+	        }
+	        else
+	        {
+	        	System.out.println(i);
+	        }
+			
+			
+			
+			
+		}
+		
+		
+		
+
+	}
+
+}
